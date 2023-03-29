09b17a27520ead6b45e52f461160712a3ea9d53e
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-03-29 20:46:52 +0300

    import files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
+import './js/fetch';
+import './js/randomNum';
+import './js/renderGallery';
+import NiceSelect from 'nice-select2/src/js/nice-select2';
 
-const URL_FOR_RANDOM = 'https://pokeapi.co/api/v2/ability/';
-const URL_FOR_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
+const selectItem = document.querySelector('.header__select')
 
-const fetchPokemons = async (limitNum, offsetNum) => {
-    const searchFilters = new URLSearchParams({
-        limit: limitNum,
-        offset: offsetNum
-    })
-    const response = await axios.get(`${URL_FOR_RANDOM}?${searchFilters}`);
-    return response.data
+const renderSelect = (select) => {
+    if (select) {
+        NiceSelect.bind(select, {
+            searchable: true, // false
+            placeholder: 'select',
+            searchtext: '...',
+            selectedtext: 'geselecteerd',
+        });
+    }
 }
 
-const fetchOnePokemon = async (name) => {
-    const response = await axios.get(`${URL_FOR_POKEMON}/${name}`);
-    return response.data
-}
-
-fetchOnePokemon('ditto').then(r => console.log(r))
+renderSelect(selectItem)
